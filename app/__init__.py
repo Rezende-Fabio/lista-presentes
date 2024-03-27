3958d2e0287f142498049fb01fad5c52bc801529
Rezende-Fabio <rezendefabio337@gmail.com>
2024-03-27 13:38:25 -0300

    adjusting list and added DataBase

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from .configurations import Blueprint
+from .configurations import DataBase
 from flask import Flask
 
 def create_app() -> Flask:
     app = Flask(__name__)
 
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Lista.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS "] = False
+    app.config["SECRET_KEY "] = "1s2a121#ddwg%$Gfhghfd25w54e&dkd25w"
+
+    DataBase.init_app(app)
     Blueprint.registerBlueprints(app)
 
     return app
