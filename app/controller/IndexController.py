b0d9361acb632a80dae9e73e547f310f4ac0d6e0
Rezende-Fabio <rezendefabio337@gmail.com>
2024-04-02 22:07:23 -0300

    added rendering of gifts

--- app/controller/IndexController.py
+++ app/controller/IndexController.py
@@ -1,7 +1,14 @@
 from app.httpResponse.HttpResponse import HttpResonse
-
+from app.models.Presente import Presente
+import base64
 
 class IndexController(HttpResonse):
 
     def renderIndex(self):
-        return self.responseRender("index.html")
+        presentes = Presente.query.all()
+        listaPresentes = []
+        for presente in presentes:
+            listaPresentes.append({"id": presente.id, "titulo": presente.p_titulo, "imagem": base64.b64encode(presente.p_imagem).decode('utf-8')})
+
+        context = {"presentes": listaPresentes}
+        return self.responseRender("index.html", context=context)
